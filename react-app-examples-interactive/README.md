b40ac6a5cad25352cf919d6252b2f13a3760b3d0
rkrawczyszyn <rkrawczyszyn@gmail.com>
2023-11-12 22:08:21 +0100

    add complete demo project

---
+++ react-app-examples-interactive/README.md
@@ -0,0 +1,5 @@
+# CSS tips
+
+-   when you don't see an element, check it's position, display, z-index, height and width
+
+-   position: absolute breaks center by margin: 0 auto
---
+++ react-app-examples-interactive/src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { InteractiveRating } from './components/solutions/interactive-rating/InteractiveRating';
+import logo from './logo.svg';
+
+function App() {
+    return <InteractiveRating />;
+}
+
+export default App;
