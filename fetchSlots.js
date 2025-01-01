45f506473f427dc230ef470a28d64c8c815e010d
rkrawczyszyn <rkrawczyszyn@gmail.com>
2025-01-01 16:48:11 +0100

    update fetch sluts

--- fetchSlots.js
+++ fetchSlots.js
@@ -2,13 +2,25 @@ const fs = require('fs');
 const https = require('https');
 const path = require('path');
 
+const formatDate = (date) => {
+  const d = new Date(date);
+  const month = `${d.getMonth() + 1}`.padStart(2, '0');
+  const day = `${d.getDate()}`.padStart(2, '0');
+  const year = d.getFullYear();
+  return `${year}-${month}-${day}`;
+};
+
 // Configuration
 const API_BASE_URL = 'https://www.znanylekarz.pl/api/v3';
-const FILTER_DATE_START = '2024-12-29';
-const FILTER_DATE_END = '2025-01-12';
+const FILTER_DATE_START = new Date().toDateString();
+const FILTER_DATE_START_COPY = new Date();
+const FILTER_DATE_END = new Date(FILTER_DATE_START_COPY.setDate(FILTER_DATE_START_COPY.getDate() + 45)).toDateString();
 const TOKEN_FILE = path.resolve(__dirname, 'token.txt');
 const DOCTOR_DETAILS_FILE = path.resolve(__dirname, 'doctorDetails.json');
 const OUTPUT_FILE = path.resolve(__dirname, 'response.json');
+
+const formattedStart = formatDate(FILTER_DATE_START);
+const formattedEnd = formatDate(FILTER_DATE_END);
 
 // Function to read a file and return its content as a string
 function readFileSync(filePath) {
@@ -28,7 +40,7 @@ const doctorDetails = JSON.parse(readFileSync(DOCTOR_DETAILS_FILE));
 
 // Generate doctor URLs
 const doctorUrls = doctorDetails.map((doctor) => {
-  return `${API_BASE_URL}/doctors/${doctor.id}/addresses/${doctor.addressId}/slots?start=${FILTER_DATE_START}T00%3A00%3A00%2B01%3A00&end=${FILTER_DATE_END}T00%3A00%3A00%2B01%3A00&includingSaasOnlyCalendar=false&with%5B%5D=address.nearest_slot_after_end&with%5B%5D=links.book.patient&with%5B%5D=slot.doctor_id&with%5B%5D=slot.address_id&with%5B%5D=slot.address&with%5B%5D=slot.with_booked`;
+  return `${API_BASE_URL}/doctors/${doctor.id}/addresses/${doctor.addressId}/slots?start=${formattedStart}T00%3A00%3A00%2B01%3A00&end=${formattedEnd}T00%3A00%3A00%2B01%3A00&includingSaasOnlyCalendar=false&with%5B%5D=address.nearest_slot_after_end&with%5B%5D=links.book.patient&with%5B%5D=slot.doctor_id&with%5B%5D=slot.address_id&with%5B%5D=slot.address&with%5B%5D=slot.with_booked`;
 });
 
 // Function to fetch slots for each doctor using built-in https
