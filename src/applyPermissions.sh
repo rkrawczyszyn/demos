befdf1727da7f19a1b8dab93265175e4f3c54530
rkrawczyszyn <rkrawczyszyn@gmail.com>
2025-01-05 14:04:33 +0100

    w

--- src/applyPermissions.sh
+++ src/applyPermissions.sh
@@ -6,10 +6,11 @@ chmod +x /home/rkrawczyszyn/demos/dist/gitUpdate.sh && \
 chmod +x /home/rkrawczyszyn/demos/dist/gitTest.sh && \
 chmod +x /home/rkrawczyszyn/demos/dist/refreshToken.js && \
 chmod +x /home/rkrawczyszyn/demos/dist/yahoo.js && \
-chmod +x customStocks.js && \
-chmod +x fetchSlots.js && \
-chmod +x gitUpdate.sh && \
-chmod +x refreshToken.js && \
-chmod +x yahoo.js && \
 dos2unix /home/rkrawczyszyn/demos/dist/gitUpdate.sh && \
 dos2unix /home/rkrawczyszyn/demos/dist/gitTest.sh
+
+# chmod +x customStocks.js && \
+# chmod +x fetchSlots.js && \
+# chmod +x gitUpdate.sh && \
+# chmod +x refreshToken.js && \
+# chmod +x yahoo.js && \
