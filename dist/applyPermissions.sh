9110fb9923440a7fc4cae5f98c2d7b96f0d193b4
rkrawczyszyn <rkrawczyszyn@gmail.com>
2025-01-05 16:59:31 +0100

    d

--- dist/applyPermissions.sh
+++ dist/applyPermissions.sh
@@ -5,4 +5,5 @@ chmod +x /home/rkrawczyszyn/demos/dist/customStocks.js && \
 chmod +x /home/rkrawczyszyn/demos/dist/gitUpdate.sh && \
 chmod +x /home/rkrawczyszyn/demos/dist/refreshToken.js && \
 chmod +x /home/rkrawczyszyn/demos/dist/yahoo.js && \
+chmod +x /home/rkrawczyszyn/demos/dist/binanceDetect.js && \
 dos2unix /home/rkrawczyszyn/demos/dist/gitUpdate.sh
