54802ba62bc978f09c2379c246849f216cb04c64
rkrawczyszyn <rkrawczyszyn@gmail.com>
2025-01-18 21:28:31 +0100

    f

--- dist/coinPriceAlert/config/coinWatchConfig.js
+++ dist/coinPriceAlert/config/coinWatchConfig.js
@@ -5,7 +5,37 @@ exports.coinWatchConfig = [
     {
         coinCode: 'dymension',
         direction: 'GoDown',
-        value: 4.8,
+        value: 5,
+        step: 0.1,
+    },
+    {
+        coinCode: 'ethena',
+        direction: 'GoDown',
+        value: 3.55,
+        step: 0.1,
+    },
+    {
+        coinCode: 'thorchain',
+        direction: 'GoDown',
+        value: 12.8,
+        step: 0.1,
+    },
+    {
+        coinCode: 'bio-protocol',
+        direction: 'GoDown',
+        value: 1,
+        step: 0.1,
+    },
+    {
+        coinCode: 'stepn',
+        direction: 'GoDown',
+        value: 0.48,
+        step: 0.1,
+    },
+    {
+        coinCode: 'pudgy-penguins',
+        direction: 'GoDown',
+        value: 0.1,
         step: 0.1,
     },
 ];
