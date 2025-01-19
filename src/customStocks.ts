dc2e1ec8d7904479d5388eb407957cdf95f1e07e
rkrawczyszyn <rkrawczyszyn@gmail.com>
2025-01-19 13:50:52 +0100

    d

--- src/customStocks.ts
+++ src/customStocks.ts
@@ -179,7 +179,7 @@ const processCoin = async (coinInput: CoinInput): Promise<StockAnalysisResult> =
 
 async function processCoinsSequentially(coins: any[]) {
   // const safeDelayTime = calculateSafeDelay(coins.length);
-  const safeDelayTime = 5000;
+  const safeDelayTime = 6000;
   console.log(`${logDate()}: Using a delay of ${safeDelayTime / 1000} seconds between each request.`);
 
   const results = [];
